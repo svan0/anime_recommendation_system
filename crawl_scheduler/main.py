0fa34193b3a6347b228e7a7c45a72865e5594218
Marouene Sfar Gandoura <marouene.gandoura@gmail.com>
2021-11-01 00:21:26 +0100

    Fixes in crawl scheduler

--- crawl_scheduler/main.py
+++ crawl_scheduler/main.py
@@ -1,52 +1,65 @@
 import os
 import json
 
+from dotenv import load_dotenv
+
 from google.cloud import pubsub
 from google.cloud.sql.connector import connector
 
 def get_top_priority_animes(max_num_urls):
     db_conn = connector.connect(
             os.getenv("SCHEDULER_DB_INSTANCE"),
-            "pymysql",
+            "pg8000",
             user=os.getenv("SCHEDULER_DB_USER"),
             password=os.getenv("SCHEDULER_DB_PASSWORD"),
-            db=os.getenv("SCHEDULER_DB")
+            db=os.getenv("SCHEDULER_DB"),
+            port='5432'
     )
     cursor = db_conn.cursor()
     
-    list_urls = cursor.execute(
+    cursor.execute(
         f"""
             SELECT url 
             FROM anime_schedule 
             ORDER BY (last_crawl_date IS NULL) ASC, 
-                      DATEDIFF(last_inspect_date, last_crawl_date) DESC,
-                      end_date DESC, 
-                      num_watching DESC
+                     (last_inspect_date - last_crawl_date) DESC,
+                     end_date DESC, 
+                     watching_count DESC
             LIMIT {max_num_urls}
         """
     )
+    list_urls = cursor.fetchall()
+    list_urls = list([x[0] for x in list_urls])
+    cursor.close()
+    db_conn.close()
     return list_urls
 
 def get_top_priority_profiles(max_num_urls):
     db_conn = connector.connect(
             os.getenv("SCHEDULER_DB_INSTANCE"),
-            "pymysql",
+            "pg8000",
             user=os.getenv("SCHEDULER_DB_USER"),
             password=os.getenv("SCHEDULER_DB_PASSWORD"),
-            db=os.getenv("SCHEDULER_DB")
+            db=os.getenv("SCHEDULER_DB"),
+            port='5432'
     )
+
     cursor = db_conn.cursor()
     
-    list_urls = cursor.execute(
+    cursor.execute(
         f"""
             SELECT url 
             FROM profile_schedule 
             ORDER BY (last_crawl_date IS NULL) ASC,
-                     DATEDIFF(last_crawl_date, last_inspect_date) DESC,
-                     DATEDIFF(last_inspect_date, last_online_date) DESC 
+                     (last_crawl_date - last_inspect_date) DESC,
+                     (last_inspect_date - last_online_date) DESC 
             LIMIT {max_num_urls}
         """
     )
+    list_urls = cursor.fetchall()
+    list_urls = list([x[0] for x in list_urls])
+    cursor.close()
+    db_conn.close()
     return list_urls
 
 
@@ -71,7 +84,7 @@ def schedule_anime(request):
     except:
         max_num_urls = 1000
     
-    list_urls = get_top_priority_animes(1000)
+    list_urls = get_top_priority_animes(max_num_urls)
     for anime_url in list_urls:
         message = {
             'type' : 'anime',
@@ -111,4 +124,6 @@ def schedule_profile(request):
         publish_client.publish(topic_path, message)
 
 if __name__ == '__main__':
+    load_dotenv()
     print(get_top_priority_profiles(10))
+    print(get_top_priority_animes(10))
