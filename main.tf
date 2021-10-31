7e8209941a596a385ac30ff3489f544b8ec1b155
Marouene Sfar Gandoura <marouene.gandoura@gmail.com>
2021-10-31 01:55:32 +0100

    Update terraform db + db user

--- main.tf
+++ main.tf
@@ -28,9 +28,9 @@ resource "google_storage_bucket_object" "scheduler_cloud_functions_artifact" {
 }
 
 resource "google_sql_database_instance" "crawl_scheduler_db_instance" {
-  name             = "scheduler-db-instance-dev-small"
+  name             = "scheduler-db-instance-dev-small-2"
   region           = "us-central1"
-  database_version = "MYSQL_5_7"
+  database_version = "POSTGRES_13"
   settings {
     availability_type = "ZONAL"
     tier              = "db-custom-1-3840"
@@ -39,6 +39,11 @@ resource "google_sql_database_instance" "crawl_scheduler_db_instance" {
 resource "google_sql_database" "crawl_scheduler_db" {
   name     = "scheduler_db"
   instance = google_sql_database_instance.crawl_scheduler_db_instance.name
+}
+resource "google_sql_user" "user" {
+  name     = "root"
+  instance = google_sql_database_instance.crawl_scheduler_db_instance.name
+  password = "1123581321"
 }
 
 resource "google_pubsub_topic" "anime_data_topic" {
